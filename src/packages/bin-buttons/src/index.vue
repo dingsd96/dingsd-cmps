9f02f01be42f87453c0b641266fdf85804dd5439
binbin07 <1564733642@qq.com>
2020-04-28 09:51:40 +0800

    搭建环境

---
+++ src/packages/bin-buttons/src/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="binbin">
+        <p>1111111</p>
+        <el-button class="btn" type="primary" round>主要按钮</el-button>
+    </div>
+</template>
+<script>
+export default {
+    name:'bin-buttons',
+    created() {
+        
+    },
+}
+</script>
+ <style lang="scss">
+    
+    .binbin{
+        background-color: #032;
+        .btn{
+            
+        }
+    }
+
+</style>
+
